727e60bcda5f44dfdbe268c14136a1822ac7fdb5
Alex Buaiscia <alex.buaiscia@gmail.com>
2022-01-21 21:28:18 +0100

    feat: starting out first pages

---
+++ pages/houses/[id].js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+
+import houses from '../../houses.js'
+import Layout from '../../components/Layout'
+
+
+
+export default function House(props) {
+  return (
+    <Layout
+      content={
+        <div>
+          <Head>
+            <title>{props.house.title}</title>
+          </Head>
+          <article>
+            <img src={props.house.picture} width="100%" alt="House picture" />
+            <p>
+              {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+          </article>
+          <aside></aside>
+        </div>
+      }
+    />
+
+  )
+}
+
+export async function getServerSideProps({ query }) {
+  const { id } = query
+  return {
+    props: {
+      house: houses.filter((house) => house.id === parseInt(id))[0]
+    }
+  }
+}
